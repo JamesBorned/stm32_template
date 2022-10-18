4684351204e77e741842127168a8c2f21c6064fa
Alena Isaicheva <Alencha5555555@gmail.com>
2022-10-18 19:28:05 +0200

    changed from c13 to b13 frequency

--- main.c
+++ main.c
@@ -10,16 +10,16 @@ void delay(uint32_t ticks) {
 int __attribute((noreturn)) main(void) {
 	// Enable clock for AFIO
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-	// Enable clock for GPIOC
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
-	// Enable PC13 push-pull mode
-	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+	// Enable clock for GPIOb
+	RCC->APB2ENR |= RCC_APB2ENR_IOPBEN;
+	// Enable Pb13 push-pull mode
+	GPIOB->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
+	GPIOB->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
 
     while (1) {
-	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
-		delay(1000000);
-	    GPIOC->ODR &= ~(1U<<13U);
+	    GPIOB->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
+		delay(100000);
+	    GPIOB->ODR &= ~(1U<<13U);
 	    delay(1000000);
     }
 }
